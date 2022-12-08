2cbaf1930c252a7158eb47add0212fd058fe99ab
MJ Sung <themrsung@gmail.com>
2022-12-08 16:20:34 +0900

    d

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import "./style/Home.css"
+
+const Home = () => {
+    return <h1>Home</h1>
+}
+
+export default Home
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,7 @@
+import "./style/Landing.css"
+
+const Landing = () => {
+    return <h1>Landing</h1>
+}
+
+export default Landing
