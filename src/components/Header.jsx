0f397193283e664298602fe9b602bc77ec76f9ee
MJ Sung <themrsung@gmail.com>
2022-12-09 09:55:48 +0900

    source code info

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,16 @@ const Header = (props) => {
     return (
         <header className="Header">
             <h1>TODO-LIST</h1>
-            <p>{props.motd}</p>
+            <p>
+                {props.motd}
+                <br />
+                <span className="HeaderCreditInfo">
+                    Made in Korea |{" "}
+                    <a href="https://github.com/themrsung/todo-router-2">
+                        Source Code
+                    </a>
+                </span>
+            </p>
         </header>
     )
 }
