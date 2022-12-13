35c76c482c0be3dc6f2d5908c3f5203164123157
MJ Sung <themrsung@gmail.com>
2022-12-13 21:07:44 +0900

    03030

--- src/redux/config/configStore.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, legacy_createStore as createStore } from "redux"
-import todolist from "../modules/todolist"
-
-const rootReducer = combineReducers({
-    todolist: todolist
-})
-const store = createStore(rootReducer)
-
-export default store
